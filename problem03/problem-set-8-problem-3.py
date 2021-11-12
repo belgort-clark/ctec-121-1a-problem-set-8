8f7ed13039a684689bd9c84765de606ab77ed59e
Bruce Elgort <belgort@clark.edu>
2021-11-12 10:20:06 -0800

    Initial commit

---
+++ problem03/problem-set-8-problem-3.py
@@ -0,0 +1,26 @@
+# Module 9 - Problem Set No. 8
+# Problem 3
+
+def calculate_average(score1, score2, score3, score4, score5):
+    # your codes goes here
+
+    return avarageScore
+
+
+def determine_grade(score):
+    # your codes goes here
+
+    return letteGrade
+
+
+def main():
+    pass # remove this line of code
+
+    # get 5 test scores
+
+    # calculate and display avarage of 5 scores
+    
+    # calculate letter grade for each score and dipslay the socre and the correspdonding letter grade
+    
+
+main()
